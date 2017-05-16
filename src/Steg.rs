112878403f91e3559a6b653b5ad05887c43f6ef3
teovoinea <voinea.teodor@gmail.com>
2017-05-15 20:37:42 -0400

    skeleton for the builder pattern
    Defined the functions that will initially be available

--- src/Steg.rs
+++ src/Steg.rs
@@ -14,23 +14,79 @@ use image::{
 
 pub struct Steg {
 	input: [u8],
-	ouput: ImageBuffer<Rgba<u8>, Vec<u8>>,
+	container: DynamicImage,
 	method: Method,
+	action: Action,
 }
 
 pub enum Method {
 	Alpha,
 }
 
+pub enum Action {
+	Read,
+	Write,
+}
+
 impl Steg {
 
-	pub fn new() -> Steg {
+	pub fn new(a: Action) -> Steg {
 		Steg {
-			self.method = Method::Alpha;
-			self.input = [0x00_u8];
+			//self.method = Method::Alpha;
+			//self.input = [0x00_u8];
+			self.action = a;
 		}
 	}
 
+	pub fn input_as_file<'a>(&'a mut self, filename: String) -> &'a mut Steg {
+		let mut f = File::open(src).expect("Could not open file");
+		let mut buffer = Vec::new();
+		f.read_to_end(&mut buffer).expect("Could not read file");
+		self.input = buffer.as_slice();
+		self
+	}
+
+	pub fn input_as_string<'a>(&'a mut self, text: String) -> &'a mut Steg {
+		self.input = text.as_bytes();
+		self
+	}
+
+	pub fn input_as_bytes<'a>(&'a mut self, buffer: &[u8]) -> &'a mut Steg {
+		self.input = buffer;
+		self
+	}
+
+	pub fn encrypt<'a>(&'a mut self, key: String) -> &'a mut Steg {
+		self
+	}
+
+	pub fn decrypt<'a>(&'a mut self, ley: String) -> &'a mut Steg {
+		self
+	}
+
+	pub fn container_as_file<'a>(&'a mut self, filename: String) -> &'a mut Steg {
+		self.container = image::open(&Path::new(&filename)).unwrap();
+		self
+	}
+
+	pub fn container_as_img<'a>(&'a mut self, img: DynamicImage) -> &'a mut Steg {
+		self.container = img;
+		self
+	}
+
+	pub fn execute(&self) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+		match self.action {
+			Action::Write => {
+
+			}
+
+			Action::Read => {
+				
+			}
+		}
+	}
+
+/*
 	pub fn input<'a>(&'a mut self, bytes: &[u8]) -> &'a mut Steg{
 		self.input = bytes;
 		self
@@ -62,3 +118,11 @@ pub fn string_to_bytes(message: String) -> &[u8] {
 pub fn file_to_bytes(filename: String) -> &[u8] {
 
 }
+
+var s = Steg::new(Action::Write).input_as_file("my_file.png").encrypt("some_key").execute().output_as_bytes();
+var ns = Steg::new(Action::Read).input_as_bytes(Vec<u8>).decrypt("some_key").execute().output_as_file("my_file.png");
+
+input_as_x => turns things into bytes
+encrypt/decrypt applies the functions to the input bytes
+
+*/
